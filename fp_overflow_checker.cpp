528b92cfe5eed273f11929ea2a0688fa2db1300c
Jaime Martinez <jjmtz10@yahoo.com>
2026-03-27 21:28:18 -0500

    Basix program shell and command line argument validation

--- fp_overflow_checker.cpp
+++ fp_overflow_checker.cpp
@@ -1,3 +1,12 @@
-//
-// Created by jjmtz on 3/27/2026.
-//
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cout << std::endl;
+        std::cout << "    loop_bound is a positive floating-point value" << std::endl;
+        std::cout << "    loop_counter is a positive floating-point value" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
