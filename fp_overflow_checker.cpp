aa8fb5ead3f068aeaebcf8df2c47f2a412c60061
Jaime Martinez <jjmtz10@yahoo.com>
2026-03-27 21:41:05 -0500

    Created loop bound and loop counter as floating point inputs

--- fp_overflow_checker.cpp
+++ fp_overflow_checker.cpp
@@ -8,5 +8,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    float loop_bound = std::stof(argv[1]);
+    float loop_counter = std::stof(argv[2]);
+
+    std::cout << "Loop bound value: " << loop_bound << std::endl;
+    std::cout << "Loop counter value: " << loop_counter << std::endl;
+
+
     return 0;
 }
