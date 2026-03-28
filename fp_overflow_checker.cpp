65a9a4efe6714b88257a9508cca9ad70a40bc8f7
Jaime Martinez <jjmtz10@yahoo.com>
2026-03-27 21:29:59 -0500

    Basic program shell and command line argument validation

--- fp_overflow_checker.cpp
+++ fp_overflow_checker.cpp
@@ -1,12 +1,3 @@
-#include <iostream>
-
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cout << std::endl;
-        std::cout << "    loop_bound is a positive floating-point value" << std::endl;
-        std::cout << "    loop_counter is a positive floating-point value" << std::endl;
-        return 1;
-    }
-
-    return 0;
-}
+//
+// Created by jjmtz on 3/27/2026.
+//
